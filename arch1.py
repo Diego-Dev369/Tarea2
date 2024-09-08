580aabbad17dd32734a59fc36e5e21218fb30b2b
Diego Axel Contreras Guerrero <diegoaxelcontreras13@gmail.com>
2024-09-07 22:19:38 -0600

    Archivos python segunda rama

---
+++ arch1.py
@@ -0,0 +1,7 @@
+class Celular():  #Las clases deben ser con Pascal Case
+    marca = "Motorola"
+    modelo = "edge 40"
+    camara = "48MP"
+    
+Celular1 = Celular()  #A esto se le llama instancia
+print(Celular1.marca)
---
+++ arch2.py
@@ -0,0 +1,13 @@
+#Strings
+"Hola"
+'Hola' #Estos dos son para una sola linea
+
+"""Hola"""
+'''Hola'''#Mientras que estos otros son para multiples lineas
+
+#Enteros y flotantes(decimales)
+40  #int
+40.2  #Float
+
+#Boolean
+#True y False
